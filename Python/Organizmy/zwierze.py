257c89e5449923b754e698a1b9847f0456afa6bb
SzymonZebrowski <szymekze@gmail.com>
2018-06-09 23:21:12 +0200

    Animal- working fine
    Out of array and NoneType problem

--- Python/Organizmy/zwierze.py
+++ Python/Organizmy/zwierze.py
@@ -41,7 +41,7 @@ class Zwierze(Organizm):
                     self._ref.makeOrganizm(tmpX, tmpY, self._symbol)
                     break
                 else:
-                    posibilities.remove(kierunek)
+                    del(possibilities[kierunek])
 
         elif self._ref.getOrganizmAtXY(comingX, comingY).getSila() >= self._sila:
             self._ref.moveOrganizm(comingX, comingY, self._x, self._y)
